81f263fc2db47fb1420ab85468d8e6bb4e3d23a9
oleksandr.arzamastsev <sarzamastsev5@gmail.com>
2022-12-29 18:46:47 +0200

    Initial commit

---
+++ src/main/java/com/booster/cliclient/BoosterCliClientApplication.java
@@ -0,0 +1,13 @@
+package com.booster.cliclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoosterCliClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoosterCliClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/booster/cliclient/BoosterCliClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booster.cliclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoosterCliClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
