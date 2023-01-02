5db5f1c78dddb86a12005b93d9717947df121fd8
oleksandr.arzamastsev <sarzamastsev5@gmail.com>
2023-01-02 17:32:35 +0200

    feat: Implement a command to add a new vocabulary entry

---
+++ src/test/java/com/booster/cliclient/BaseIntegrationTest.java
@@ -0,0 +1,33 @@
+package com.booster.cliclient;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.mockserver.integration.ClientAndServer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@SpringBootTest
+public abstract class BaseIntegrationTest {
+
+    protected static ClientAndServer mockServer;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+    @Autowired
+    protected BoosterCliLauncher boosterCliLauncher;
+    @MockBean
+    protected UserInputReader adapter;
+
+    @BeforeAll
+    public static void beforeAll() {
+        mockServer = ClientAndServer.startClientAndServer(8081);
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        mockServer.stop();
+    }
+
+}
